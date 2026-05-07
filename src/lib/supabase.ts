1fa12b426a920cbb321fdc3056399c5ae6d45285
Lucas Gasparello <lucas.gasparello@3sat.com.br>
2026-05-07 19:30:37 -0300

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  import.meta.env.VITE_SUPABASE_URL ?? "https://zoyjotnvxfcelzgvhwnh.supabase.co",
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpveWpvdG52eGZjZWx6Z3Zod25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjk2ODAsImV4cCI6MjA5Mzc0NTY4MH0.BPkqD-fe6E48W8wC8Od49-enX9arJePqoO0z4ZNmOlU",
 );
